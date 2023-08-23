c810bd469edf256f2e84dd00233872e014b185b7
xiaou <3153566913@qq.com>
2023-08-23 16:02:42 +0800

    8.23

---
+++ nav/js/bottom.js
@@ -0,0 +1,12 @@
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+one.addEventListener('click',()=>{
+    window.open("https://effect.guoyaxue.top/nav/index.html")
+})
+two.addEventListener('click',()=>{
+    window.open("https://effect.guoyaxue.top/rose/love2/index.html")
+})
+three.addEventListener('click',()=>{
+    window.open("https://effect.guoyaxue.top/rose/love3/index.html")
+})
